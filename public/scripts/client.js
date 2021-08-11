347819a04dd1588bf3a6202af59b9b5cf1ecec0a
Anthony Kim <fbssaejun@gmail.com>
2021-08-11 19:47:25 +0000

    added escape function to prevent XXS attacks

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,26 +1,8 @@
-//POST request for new tweet submittions to '/tweets' path
-const submitTweets = function () {
-  $("form").on("submit", function (event) {
-    event.preventDefault();
-    const tweetText = $("#tweet-text").val();
-    if (!tweetText) {
-      alert("There is no text to be submitted!");
-    } else if (tweetText.length > 140) {
-      alert("You have exceeded maximum charaters!");
-    } else {
-      $.post("/tweets", $(this).serialize());
-    }
-  });
-};
-
-//GET request for loading tweets from the database
-const loadTweets = function () {
-  $.get("/tweets")
-    .then((data) => {
-      renderTweets(data);
-      timeago.render(document.querySelectorAll(".posts"));
-    })
-    .catch((e) => console.log(("Error: ", e)));
+//Escapes from XSS attacks
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
 };
 
 //Returns an html formate with information from given data
@@ -31,7 +13,7 @@ const createTweetElement = function (tweet) {
       <label for=""><img src=${tweet.user.avatars} />${tweet.user.name}</label>
       <a href="">${tweet.user.handle}</a>
     </header>
-    <p>${tweet.content.text}</p>
+    <p>${escape(tweet.content.text)}</p>
     <footer>
     <div>
       <span class="posts">Posted : ${timeago.format(new Date())}</span>
@@ -47,14 +29,50 @@ const createTweetElement = function (tweet) {
   return $tweet;
 };
 
+//Function that resets text box and text counter after new submittion
+const resetText = () => {
+  $("#tweet-text").val('');
+  $('.counter').val(140);
+};
+
+//POST request for new tweet submittions to '/tweets' path
+const submitTweets = function () {
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    const tweetText = $("#tweet-text").val();
+    if (!tweetText) {
+      alert("There is no text to be submitted!");
+    } else if (tweetText.length > 140) {
+      alert("You have exceeded maximum charaters!");
+    } else {
+      $.post("/tweets", $(this).serialize()).then(() => {
+        loadTweets();
+        resetText();
+      });
+    }
+  });
+};
+
+//GET request for loading tweets from the database
+const loadTweets = function () {
+  $.get("/tweets")
+    .then((data) => {
+      renderTweets(data);
+      timeago.render(document.querySelectorAll(".posts"));
+    })
+    .catch((e) => console.log(("Error: ", e)));
+};
+
 //Render tweets in an html format
 const renderTweets = function (tweets) {
+  //clears previously posted tweets from page
+  $(".posted-tweets").empty();
   // loops through tweets
   for (tweet of tweets) {
     // calls createTweetElement for each tweet
     const newTweet = createTweetElement(tweet);
     // takes return value and appends it to the tweets container
-    $(".posted-tweets").append(newTweet);
+    $(".posted-tweets").prepend(newTweet);
   }
 };
 
