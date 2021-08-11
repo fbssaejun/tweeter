4f6a8cb61358d619efea4c5e2914312c9fbe630f
Anthony Kim <fbssaejun@gmail.com>
2021-08-11 22:18:17 +0000

    added function for scroller button

---
+++ public/scripts/composer.js
@@ -0,0 +1,28 @@
+//Checks character counts for new tweet submittions
+$(document).ready(function() {
+  $('#tweet-text').on('input', (e) => {
+    const counter = e.target.value.length
+    $('.counter').text(140 - counter)
+    if(counter > 140) {
+      $('.counter').css('color', 'red')
+    } else {
+      $('.counter').css('color', 'black')
+    }
+  })
+  //Calls scroller function
+  scroller();
+});
+
+//Goes to the textarea of the page when arrow is clicked
+const scroller = function () {
+  $(window).scroll(function() {
+    //Displays the button, fades out after 5seconds
+    $( ".scroll" ).css('display', 'inline').fadeOut(5000);
+  });
+
+  $(".scroll").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".new-tweet").offset().top
+    }, 1000);
+  });
+}
