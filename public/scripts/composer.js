beb1e6b30a132fa5446fe8a1d79b93b00d208190
Anthony Kim <fbssaejun@gmail.com>
2021-08-12 16:33:18 +0000

    lint fix

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,28 +1,28 @@
 //Checks character counts for new tweet submittions
 $(document).ready(function() {
   $('#tweet-text').on('input', (e) => {
-    const counter = e.target.value.length
-    $('.counter').text(140 - counter)
-    if(counter > 140) {
-      $('.counter').css('color', 'red')
+    const counter = e.target.value.length;
+    $('.counter').text(140 - counter);
+    if (counter > 140) {
+      $('.counter').css('color', 'red');
     } else {
-      $('.counter').css('color', 'black')
+      $('.counter').css('color', 'black');
     }
-  })
+  });
   //Calls scroller function
   scroller();
 });
 
 //Goes to the textarea of the page when arrow is clicked
-const scroller = function () {
+const scroller = function() {
   $(window).scroll(function() {
     //Displays the button, fades out after 5seconds
-    $( ".scroll" ).css('display', 'inline')
+    $(".scroll").css('display', 'inline').fadeOut(5000);
   });
 
   $(".scroll").click(function() {
     $('html, body').animate({
-        scrollTop: $(".new-tweet").offset().top
+      scrollTop: $(".new-tweet").offset().top
     }, 1000);
   });
-}
+};
